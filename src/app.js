ef715033b9f4ecd652b4159d8f7037239a91159b
palmani <Nicolas_Palmar@manulife.ca>
2021-01-14 17:49:56 -0500

    get users and accounts from an external file and dispay in index

--- src/app.js
+++ src/app.js
@@ -9,8 +9,16 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
+// read the account data from a file to a js object
+const accountData = fs.readFileSync("src/json/accounts.json", { encoding: "utf8"});
+const accounts = JSON.parse(accountData);
+
+// read the user data from a file to a js object
+const userData = fs.readFileSync("src/json/users.json", { encoding: "utf8"});
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
-    res.render("index", {title: 'Index'});
+    res.render("index", {title: 'Account Summary', accounts: accounts});
   });
 
 app.listen(3000, () => {
